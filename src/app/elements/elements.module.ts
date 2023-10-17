f73d0e225094a5ab1a4b2716a421f13720250df1
Chu Văn Giá <vangiachu@gmail.com>
2023-10-17 18:01:06 +0700

    commit chieu 17.10.2023

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     ElementsHomeComponent,
-    PlaceholderComponent
+    PlaceholderComponent,
+    TimesDirective
   ],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ], 
   exports: []
 })
--- src/app/elements/placeholder/placeholder.component.ts
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-placeholder',
   templateUrl: './placeholder.component.html',
   styleUrls: ['./placeholder.component.css']
 })
-export class PlaceholderComponent {
+export class PlaceholderComponent implements OnInit {
+  @Input() header = true;
+  @Input() lines = 3;
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
