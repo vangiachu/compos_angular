a3863dea96a237a9fdfe0f36e4a8c1e64da7d88a
Chu Văn Giá <vangiachu@gmail.com>
2023-10-17 17:20:04 +0700

    fix git remote commit

---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+
+
+@NgModule({
+  declarations: [
+    ElementsHomeComponent,
+    PlaceholderComponent
+  ],
+  imports: [
+    CommonModule,
+    ElementsRoutingModule
+  ], 
+  exports: []
+})
+export class ElementsModule { }
