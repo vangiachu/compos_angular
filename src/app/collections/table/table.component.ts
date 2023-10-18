98f2bae18f60200a10e39042757420232a610be4
Chu Văn Giá <vangiachu@gmail.com>
2023-10-18 11:41:11 +0700

    add component routing

---
+++ src/app/collections/table/table.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+  @Input('class') classNames = '';
+  @Input() data: any[] = [];
+  @Input() headers: any[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+}
